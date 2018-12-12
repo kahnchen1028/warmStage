24472034cae0fc06b4a1c615131f540e71749f4e
KahnChen <kahnchen1028@gmail.com>
2018-12-12 19:13:11 +0800

    Add test websocket feature

--- src/app/componments/layout/footer/footer.component.ts
+++ src/app/componments/layout/footer/footer.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-
+export class FooterComponent implements OnInit, OnDestroy {
+  message: String;
+  subject: WebSocketSubject<any>;
   constructor() { }
 
   ngOnInit() {
+
+    this.subject = webSocket({ url: 'ws://localhost:5848', protocol: 'echo-protocol' });
+    this.subject.subscribe(
+      (msg) => this.message = msg.message,
+      (err) => { console.log(err) },
+      () => console.log('complete'))
+  }
+  send() {
+    this.subject.next({ message: 'some message from client' });
   }
 
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.subject.unsubscribe();
+  }
 }
